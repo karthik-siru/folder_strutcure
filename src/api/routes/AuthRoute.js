7f5c8412bb2ca26be3e15a3e17cd55ce342a8ae0
karthik-siru <karthiksiruvalam@gmail.com>
2022-12-16 19:26:13 +0530

    changes to authroutes.js

--- src/api/routes/AuthRoute.js
+++ src/api/routes/AuthRoute.js
@@ -1,8 +1,18 @@
-const express = require('express');
-const authController = require('../controllers/AuthController');
-const auth = require('../middlewares/auth');
+const express = require("express");
+const authController = require("../controllers/AuthController");
+const auth = require("../middlewares/auth");
 const router = express.Router();
 
+/**
+ * @openapi
+ * /me:
+ *   get:
+ *     tags: [auth]
+ *     responses:
+ *       200:
+ *         description: user information
+ */
+router.post("/login", authController.login);
 
 /**
  * @openapi
@@ -13,7 +23,7 @@ const router = express.Router();
  *       200:
  *         description: user information
  */
-router.post('/login',authController.login);
+router.post("/logout", auth(), authController.logout);
 
 /**
  * @openapi
@@ -24,7 +34,7 @@ router.post('/login',authController.login);
  *       200:
  *         description: user information
  */
-router.post('/logout', auth(), authController.logout);
+router.post("/refresh-tokens", authController.refreshTokens);
 
 /**
  * @openapi
@@ -35,7 +45,7 @@ router.post('/logout', auth(), authController.logout);
  *       200:
  *         description: user information
  */
-router.post('/refresh-tokens', authController.refreshTokens);
+router.post("/forgot-password", authController.forgotPassword);
 
 /**
  * @openapi
@@ -46,17 +56,6 @@ router.post('/refresh-tokens', authController.refreshTokens);
  *       200:
  *         description: user information
  */
-router.post('/forgot-password', authController.forgotPassword);
-
-/**
- * @openapi
- * /me:
- *   get:
- *     tags: [auth]
- *     responses:
- *       200:
- *         description: user information
- */
-router.post('/reset-password', authController.resetPassword);
+router.post("/reset-password", authController.resetPassword);
 
 module.exports = router;

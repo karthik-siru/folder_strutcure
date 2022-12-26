75209847d7c7cab90fa1c3553a212dda31248f8f
karthik-siru <karthiksiruvalam@gmail.com>
2022-12-26 20:37:32 +0530

    db bug fixed

--- src/api/routes/student.js
+++ src/api/routes/student.js
@@ -3,10 +3,7 @@ const router = express.Router();
 
 // middleware
 const { getStudentById } = require("../middlewares/student");
-<<<<<<< HEAD
 const { studentAuth } = require("../middlewares/auth");
-=======
->>>>>>> b4a64c54736777ffa219c02bac675b1431501661
 //controllers
 const {
   registerStudent,
@@ -32,18 +29,6 @@ router.param("rollno", getStudentById);
 
 // routes
 router.post("/login", Login);
-<<<<<<< HEAD
-router.post("/register", registerStudent);
-router.post("/forgot-password/:rollno", studentAuth(), forgotPassword);
-router.post("/change-password/:rollno", studentAuth(), changePassword);
-router.get("/get-student-by-rollno", studentAuth(), getStudentByRollno);
-router.get(
-  "/get-student-by-partial-name",
-  studentAuth(),
-  getStudentByPartialName
-);
-router.get("/get-all-students", getAllStudents);
-=======
 
 router.post("/register", auth([2, hostelAdmin]), registerStudent);
 router.post("/forgot-password/:rollno", forgotPassword);
@@ -71,6 +56,5 @@ router.get(
   auth([6, messAdmin, hostelAdmin, hostelWarden, careTaker, hostelSecretary]),
   getAllStudents
 );
->>>>>>> b4a64c54736777ffa219c02bac675b1431501661
 
 module.exports = router;
